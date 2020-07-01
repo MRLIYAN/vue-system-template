90e1d1f697100c8edbb7e40b030cb6bfc941b9a6
liyan <347147641@qq.com>
2020-07-01 15:41:33 +0800

    增加element-ui

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,46 @@
 <template>
    <div class="login">
+      <div class="ctaim">
+         <div class="input-group input-group1">
+            <img src="user" alt="">
+            <input type="text" id="username">
+         </div>
+         <div class="input-group input-group2">
+            <img src="user" alt="">
+            <input type="text" id="pwd">
+         </div>
+         <input type="button" id="login-btn" value="登录" @click="login">
 
+         <div class="tip">
+            <span>用户名：{{loginmsg.username}}</span>
+         </div>
+      </div>
    </div>
 </template>
 
 <script>
 export default {
-   
+   data(){
+      return {
+         loginmsg:{
+            username:'admin',
+            password:'admin'
+         }
+
+      }
+   },
+   methods: {
+      login(){
+
+      }
+   },
 }
 </script>
 
 <style lang="scss" scoped>
+.login{
+   width:100%; 
+   height:100%;
 
+}
 </style>
