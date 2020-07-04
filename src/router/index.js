d2f00e9d695b7e8365117b8d0f65faaf20ec10f5
liyan <347147641@qq.com>
2020-07-04 18:39:05 +0800

    增加路由判断

--- src/router/index.js
+++ src/router/index.js
@@ -26,6 +26,11 @@ Vue.use(VueRouter)
     path:'/layout',
     name:'layout',
     component:()=>import('@/views/layout')
+  },
+  {
+    path:'/404',
+    name:'404',
+    component:()=>import('@/views/404')
   }
 ]
 
@@ -34,5 +39,20 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.length ===0) {//如果未匹配到路由
+    from.name ? next({ name:from.name}) : next('/404'); //如果上级也未匹配到路由则跳转登录页面，如果上级能匹配到则转上级路由
+  } else {
+    next(); //如果匹配到正确跳转
+  }
+});
+
+
+// router.onError(callback=>{
+//   router.push({path:'/404'})
+//   throw new Error(callback);
+// })
+
 
 export default router
