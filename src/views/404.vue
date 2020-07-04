e923fea5be3f47a513d8d51470efed6731e443c5
liyan <347147641@qq.com>
2020-07-04 20:24:07 +0800

    修改404页面

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,5 +1,5 @@
 <template>
-   <div style="width:100%; height:100%; overflow:hidden;">
+   <div style="width:100%; height:100%;">
       <img src="@/assets/images/error/404.gif" style="height:100%;">
    </div>
 </template>
