c74600b9d63330bbbd0bed5ded09fe351e8aaec5
liyan <li-yan@bmsoft.com.cn>
2020-12-04 14:36:35 +0800

    完善路由信息，完善面包屑

--- src/views/layout/components/slideMenu/slideMenuTree.vue
+++ src/views/layout/components/slideMenu/slideMenuTree.vue
@@ -5,13 +5,21 @@
                 没有重定向并且也没有children的则也没有子级菜单，直接显示，否则取children循环
              -->
             <div v-if="routeItem.redirect || !routeItem.redirect && !routeItem.children">
-                <!-- 有重定向，显示重定向的路径，没有，则显示默认的path，为了能够和$route.path对应，改变menu菜单组件激活的状态颜色 -->
-                <el-menu-item :index="routeItem.redirect ? routeItem.redirect : routeItem.path">
+                <!-- 有重定向，则说明需要调转到重定向的路由，则应该显示重定向的路由的信息 -->
+                <el-menu-item v-if="routeItem.redirect" :index="routeItem.children[0].path">
+                    <i class="menu-icon" :class="routeItem.children[0].meta.icon||''" v-if="routeItem.children[0].meta.icon"></i>
+                    <span class="menu-font" :title="routeItem.children[0].meta.title||''" slot="title">{{routeItem.children[0].meta.title}}</span>
+                </el-menu-item>
+                <!-- 没有重定向，则是正常要显示的菜单，显示-->
+                <el-menu-item v-else :index="routeItem.path">
                     <i class="menu-icon" :class="routeItem.meta.icon||''" v-if="routeItem.meta.icon"></i>
                     <span class="menu-font" :title="routeItem.meta.title||''" slot="title">{{routeItem.meta.title}}</span>
                 </el-menu-item>
             </div>
+
+            <!-- 有自己菜单，递归循环自己的组件 -->
             <el-submenu v-else :index="routeItem.path">
+                <!-- 这里就是显示的有子级菜单的可折叠的路由，带箭头折叠的路由信息 -->
                 <template slot="title">
                     <i class="menu-icon" :class="routeItem.meta.icon||''" v-if="routeItem.meta.icon"></i>
                     <span class="menu-font" :title="routeItem.meta.title||''" :data-path="routeItem.path">{{routeItem.meta.title}}</span>
