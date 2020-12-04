0198f8e3e9c348170bbfd2eecb62d1bdce047721
liyan <li-yan@bmsoft.com.cn>
2020-12-04 16:17:51 +0800

    删除部分无用判断

--- src/views/layout/components/slideMenu/slideMenuTree.vue
+++ src/views/layout/components/slideMenu/slideMenuTree.vue
@@ -7,12 +7,12 @@
             <div v-if="routeItem.redirect && routeItem.redirect != 'no' || !routeItem.redirect && !routeItem.children">
                 <!-- 有重定向，则说明需要调转到重定向的路由，则应该显示重定向的路由的信息 -->
                 <el-menu-item v-if="routeItem.redirect && routeItem.reidrect != 'no'" :index="routeItem.children[0].path">
-                    <i class="menu-icon" :class="routeItem.children[0].meta.icon||''" v-if="routeItem.children[0].meta.icon"></i>
+                    <i class="menu-icon" :class="routeItem.children[0].meta.icon||''" ></i>
                     <span class="menu-font" :title="routeItem.children[0].meta.title||''" slot="title">{{routeItem.children[0].meta.title}}</span>
                 </el-menu-item>
                 <!-- 没有重定向，则是正常要显示的菜单，显示-->
                 <el-menu-item v-else :index="routeItem.path">
-                    <i class="menu-icon" :class="routeItem.meta.icon||''" v-if="routeItem.meta.icon"></i>
+                    <i class="menu-icon" :class="routeItem.meta.icon||''"></i>
                     <span class="menu-font" :title="routeItem.meta.title||''" slot="title">{{routeItem.meta.title}}</span>
                 </el-menu-item>
             </div>
@@ -21,7 +21,7 @@
             <el-submenu v-else :index="routeItem.path">
                 <!-- 这里就是显示的有子级菜单的可折叠的路由，带箭头折叠的路由信息 -->
                 <template slot="title">
-                    <i class="menu-icon" :class="routeItem.meta.icon||''" v-if="routeItem.meta.icon"></i>
+                    <i class="menu-icon" :class="routeItem.meta.icon||''" ></i>
                     <span class="menu-font" :title="routeItem.meta.title||''" :data-path="routeItem.path">{{routeItem.meta.title}}</span>
                 </template>
                 <slideMenuTree v-for="child in routeItem.children" :routeItem="child" :key="child.path"></slideMenuTree>
